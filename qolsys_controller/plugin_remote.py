af94e43462f1d2334aac7dc6038c1e025b3871a7
Eric Hylands <eric.hylands@gmail.co>
2025-08-20 23:38:06 -0400

    ping

--- qolsys_controller/plugin_remote.py
+++ qolsys_controller/plugin_remote.py
@@ -217,7 +217,7 @@ class QolsysPluginRemote(QolsysPlugin):
                     await self.command_connect()
                     await self.command_pairing_request()
 
-                    #await self.command_pingevent()  # noqa: ERA001
+                    await self.command_pingevent()
                     #await self.command_timesync()  # noqa: ERA001
                     await self.command_sync_database()
                     await self.command_pair_status_request()
