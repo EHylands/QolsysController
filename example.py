8e54f86520323a681f08601bd0b57ecd5c85bfbd
Eric Hylands <eric.hylands@gmail.com>
2025-11-19 23:18:14 -0500

    ruff

--- example.py
+++ example.py
@@ -13,29 +13,24 @@ LOGGER = logging.getLogger(__name__)
 
 
 async def main() -> None:  # noqa: D103
-
     remote = QolsysController()
-
-    # Select plugin
-    remote.select_plugin("remote")
-
-    remote.plugin.settings.config_directory = "./config/"
-    remote.plugin.settings.panel_ip = "192.168.10.220"
-    remote.plugin.settings.plugin_ip = "192.168.10.73"
-    remote.plugin.settings.random_mac = ""  # Example: F2:16:3E:33:ED:20
+    remote.settings.config_directory = "./config/"
+    remote.settings.panel_ip = "****"
+    remote.settings.plugin_ip = "****"
+    remote.settings.random_mac = ""  # Example: F2:16:3E:33:ED:20
 
     # Additionnal remote plugin config
-    remote.plugin.check_user_code_on_disarm = False  # Check user code in user.conf file
-    remote.plugin.log_mqtt_mesages = False  # Enable for MQTT debug purposes
-    remote.plugin.auto_discover_pki = True
+    remote.settings.check_user_code_on_disarm = False  # Check user code in user.conf file
+    remote.settings.log_mqtt_mesages = False  # Enable for MQTT debug purposes
+    remote.settings.auto_discover_pki = True
 
     # Configure remote plugin
-    if not await remote.plugin.config(start_pairing=True):
+    if not await remote.config(start_pairing=True):
         LOGGER.debug("Error Configuring remote plugin")
         return
 
     try:
-        await remote.plugin.start_operation()
+        await remote.start_operation()
 
     except QolsysMqttError:
         LOGGER.debug("QolsysMqttError")
@@ -46,64 +41,38 @@ async def main() -> None:  # noqa: D103
     except QolsysSqlError:
         LOGGER.debug("QolsysSqlError")
 
-    if not remote.plugin.connected:
+    if not remote.connected:
         LOGGER.error("Panel not ready for operation")
         return
 
     LOGGER.debug("Qolsys Panel Ready for operation")
 
-    #await asyncio.sleep(5)
-
-    #await remote.plugin.stop_operation()
-    #LOGGER.debug("Qolsys Panel - Stopped")
-
-    # Change Z-Wave dimmer
-    # node_id: z-wane device id
-    # level: 0-99, -1 to switch from off to previous on dimmer level
-    # await asyncio.sleep(3)
-    # await remote.plugin.command_zwave_switch_multi_level(node_id=6,level=99)
-
-    # DISARM
-    #await asyncio.sleep(3)
-    #await remote.plugin.command_disarm(partition_id="0",
-    #                                   user_code="1111")
-
-    # ARM_STAY
-    #await asyncio.sleep(3)
-    #await remote.plugin.command_arm(partition_id="0",
-    #                                arming_type="ARM-STAY",
-    #                               user_code="1111",
-    #                                exit_sounds=False,
-    #                                instant_arm=True,
-    #                                entry_delay=False)
-
-
-    # ARM_AWAY
-    #await asyncio.sleep(3)
-    #await remote.plugin.command_arm(partition_id='0',
-    #                               arming_type="ARM-STAY",
-    #                               user_code="1111",
-    #                                exit_sounds=False,
-    #                               instant_arm=True)
-
-    # DISARM
-    #await asyncio.sleep(5)
-    #await remote.plugin.command_disarm(partition_id="0", user_code="1111", silent_disarming=True)
-
-    #await asyncio.sleep(5)
-    #await remote.plugin.command_execute_scene(scene_id="3")
-
-    #await asyncio.sleep(5)
-    #await remote.plugin.command_zwave_switch_multi_level(6,80)
-    #await remote.plugin.command_zwave_doorlock_set(node_id="7",locked=True)
+    await asyncio.sleep(5)
+    # await remote.command_zwave_switch_multi_level(node_id="6",level=99)
+    # await remote.command_zwave_switch_binary_set(node_id="8", status=True)
+    # await remote.plugin.command_arm(
+    #   partition_id='0',
+    #   arming_type=PartitionArmingType:ARM_NIGHT,
+    #   user_code="1111",
+    #   exit_sounds=False,
+    #   instant_arm=True)
+    # await remote.command_disarm(partition_id="0", user_code="1111")
+    # await remote.command_panel_execute_scene(scene_id="3")
+    # await remote.command_zwave_doorlock_set(node_id="7",locked=True)
+    # await remote.stop_operation()
 
     # Use an asyncio.Event to keep the program running efficiently
     stop_event = asyncio.Event()
     await stop_event.wait()
 
+
 # Change to the "Selector" event loop if platform is Windows
 if sys.platform.lower() == "win32" or os.name.lower() == "nt":
-    from asyncio import WindowsSelectorEventLoopPolicy, set_event_loop_policy
+    from asyncio import (  # type: ignore[attr-defined]
+        WindowsSelectorEventLoopPolicy,
+        set_event_loop_policy,
+    )
+
     set_event_loop_policy(WindowsSelectorEventLoopPolicy())
 
 asyncio.run(main())
