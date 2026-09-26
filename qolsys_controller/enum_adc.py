0f6ff95c2f4aaf22f0e33ebec2f8e5638b278b7d
Eric Hylands <eric.hylands@gmail.com>
2026-09-25 21:23:31 -0400

    feat: adc thermostat function name

--- qolsys_controller/enum_adc.py
+++ qolsys_controller/enum_adc.py
@@ -14,6 +14,18 @@ class vdFuncName(Enum):
     LOCK_UNLOCK = "Lock/Unlock"
     OFF_ON = "Off/On"
     MALFUNCTION = "Malfunction"
+    HEAT_SETPOINT = "Heat Setpoint"
+    MIN_HEAT_SETPOINT_LIMIT = "Min Heat Setpoint Limit"
+    MAX_HEAT_SETPOINT_LIMIT = "Max Heat Setpoint Limit"
+    COOL_SETPOINT = "Cool Setpoint"
+    MIN_COOL_SETPOINT_LIMIT = "Min Cool Setpoint Limit"
+    MAX_COOL_SETPOINT_LIMIT = "Max Cool Setpoint Limit"
+    THERMOSTAT_SYSTEM_MODE = "Thermostat System Mode"
+    FAN_MODE = "Fan Mode"
+    TEMPERATURE_UNITS = "Temperature Units"
+    THERMOSTAT_SYSTEM_MODES_SUPPORTED = "Thermostat System Modes Supported"
+    LOCAL_TEMPERATURE = "Local Temperature"
+    HUMIDITY_IN_PERCENTAGE = "Humidity In Percentage"
 
 
 class vdFuncLocalControl(IntEnum):
